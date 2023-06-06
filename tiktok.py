4365728670be6c830d25cd0bac0aa54d660fd46d
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-06 08:21:42 +0530

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -3,12 +3,12 @@ link ="https://vt.tiktok.com/ZSdxneX78/?k=1 "
 def tk(link):
     import requests
     import json
-    url = "https://tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com/vid/index"
+    url = "https://tiktok-full-info-without-watermark.p.rapidapi.com/vid/index"
 
     querystring = {"url":link}
 
     headers = {
-        "X-RapidAPI-Host": "tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com",
+        "X-RapidAPI-Host": "tiktok-full-info-without-watermark.p.rapidapi.com",
         "X-RapidAPI-Key": "3f7b85e600msh163785d28acda05p113d81jsncbb31efb5ffc"
     }
 
