12c61c465ddfe0581e33a19d8eb42f153bf07a28
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-06 08:17:22 +0530

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,5 +5,5 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = ""
+BOT_TOKEN = "6162835664:AAFKvP2ZaZj9SkCPXiv9qSpxDcHn7Vz7W2U"
 ADMINS = [852613719]  # adminlar ro'yxati
