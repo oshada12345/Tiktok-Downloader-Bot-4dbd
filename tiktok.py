867fd39cb5a2647461f86c7e483e448401d16980
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-06 08:24:10 +0530

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -10,11 +10,21 @@ def tk(link):
     headers = {
         "X-RapidAPI-Host": "tiktok-full-info-without-watermark.p.rapidapi.com",
         "X-RapidAPI-Key": "3f7b85e600msh163785d28acda05p113d81jsncbb31efb5ffc"
-    }
+    },
+       
+    };
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    const res = await axios.request(options);
+    console.log(res.data);
+    const result = {
+      videoUrl: res.data.video ? res.data.video[0] : false,
+      caption: res.data.author
+        ? res.data.author[0] + "\n @UpperDownloaderBot"
+        : false,
+    };
 
-    result = response.text
-    rest = json.loads(result)
-    return {"video":rest["video"][0],"music": rest["music"][0]}
-
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+}
